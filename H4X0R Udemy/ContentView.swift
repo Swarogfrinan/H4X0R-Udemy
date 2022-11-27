ec9db0ce55daa65a8dad1cdaebfd6e377dfaea20
Swarogfrinan <Ilyavasilev.wow@gmail.com>
2022-11-27 19:37:19 +0300

    Initial commit

---
+++ H4X0R Udemy/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  H4X0R Udemy
+//
+//  Created by Ilya Vasilev on 27.11.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ H4X0R Udemy/H4X0R_UdemyApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0R_UdemyApp.swift
+//  H4X0R Udemy
+//
+//  Created by Ilya Vasilev on 27.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct H4X0R_UdemyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
