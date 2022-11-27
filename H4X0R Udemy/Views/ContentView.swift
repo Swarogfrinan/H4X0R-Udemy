decc86947c352b1ea18e9b799c1948b9f7776345
Swarogfrinan <Ilyavasilev.wow@gmail.com>
2022-11-28 00:35:37 +0300

    re-indent

--- H4X0R Udemy/Views/ContentView.swift
+++ H4X0R Udemy/Views/ContentView.swift
@@ -3,29 +3,29 @@ import SwiftUI
 
 struct ContentView: View {
     
-   @ObservedObject var networkManager = NetworkManager()
+    @ObservedObject var networkManager = NetworkManager()
     
     var body: some View {
         
-            NavigationView {
-                List(networkManager.posts) { post in
-                        
-                    NavigationLink(destination: DetailView(url: post.url)) {
-                        HStack {
-                            Text(String(post.points))
-                            Text(post.title)
-                        }
+        NavigationView {
+            List(networkManager.posts) { post in
+                
+                NavigationLink(destination: DetailView(url: post.url)) {
+                    HStack {
+                        Text(String(post.points))
+                        Text(post.title)
                     }
-                   
                 }
-                .navigationBarTitle("H4X0R News")
                 
             }
-            .onAppear {
-                self.networkManager.fetchData()
-            }
+            .navigationBarTitle("H4X0R News")
+            
+        }
+        .onAppear {
+            self.networkManager.fetchData()
         }
     }
+}
 
 
 struct ContentView_Previews: PreviewProvider {
